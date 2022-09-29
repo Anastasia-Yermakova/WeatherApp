8fd2494ae216a8e339553f54c838e0054aa14016
Anastasia-Yermakova <nikkilungul@gmail.com>
2022-09-29 18:26:25 +0300

    switch unit bug fix

--- src/index.js
+++ src/index.js
@@ -36,6 +36,9 @@ let searchForm = document.querySelector("#search-form");
 let searchField = document.querySelector("#search-field");
 let searchButton = document.querySelector("#search-button");
 
+let tempUnit = document.querySelector("#switch-unit");
+let celsius = true;
+
 let realCelsius = 0;
 
 function displayWeather(response) {
@@ -69,6 +72,8 @@ function searchCity(event) {
   let apiKey = "9e0fb79c2f66d0cd0dcf06710976a873";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city.innerHTML}&appid=${apiKey}&units=metric`;
   axios.get(url).then(displayWeather);
+  tempUnit.innerHTML = "switch to F";
+  celsius = true;
 }
 
 searchForm.addEventListener("submit", searchCity);
@@ -89,22 +94,18 @@ function locateUser() {
 let locateMeIcon = document.querySelector("#my-location");
 locateMeIcon.addEventListener("click", locateUser);
 
-let tempUnit = document.querySelector("#switch-unit");
-let celsius = true;
-
 function convertToF(event) {
   event.preventDefault();
-  let unit = document.querySelector("#switch-unit");
   let degrees = document.querySelector("#degrees");
   let fahrenheit = Math.round((realCelsius * 9) / 5 + 32);
 
   if (celsius === true) {
     degrees.innerHTML = `${fahrenheit}&#176;F`;
-    unit.innerHTML = "switch to C";
+    tempUnit.innerHTML = "switch to C";
     celsius = false;
   } else {
     degrees.innerHTML = `${realCelsius}&#176;C`;
-    unit.innerHTML = "switch to F";
+    tempUnit.innerHTML = "switch to F";
     celsius = true;
   }
 }
